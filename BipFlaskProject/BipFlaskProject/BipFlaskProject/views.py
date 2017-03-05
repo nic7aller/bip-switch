ed60a6e33a2631578505a47a770a65ffea5fd7b2
Nic 7aller <n.7aller@gmail.com>
2017-03-04 20:45:46 -0500

    Button functionality tested

--- BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
+++ BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
@@ -1,10 +1,14 @@
 """
-Routes and views for the flask application.
+Routes and views for the flask application along with switch functions.
 """
 
 from datetime import datetime
 from flask import render_template
 from BipFlaskProject import app
+#import RPi.GPIO as GPIO
+import os
+
+lightState = 'Off'
 
 @app.route('/')
 @app.route('/home')
@@ -14,6 +18,7 @@ def home():
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        state=lightState,
     )
 
 @app.route('/contact')
@@ -35,3 +40,46 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/TurnOffSwitch')
+def turnOff():
+    """Turns off switch"""
+    ChangeLightState(0)
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+        state=lightState,
+    )
+
+@app.route('/TurnOnSwitch')
+def turnOn():
+    """Turns on switch"""
+    ChangeLightState(1)
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+        state=lightState,
+    )
+
+def ChangeLightState(onOrOff):
+    if onOrOff == 0:
+        lightState = TurnOffSwitch()
+    elif onOrOff == 1:
+        lightState = TurnOnSwitch()
+    return
+
+def TurnOffSwitch():
+    print 'Switch is off'
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(2, GPIO.OUT)
+    #GPIO.output(2, True)
+    return 'Off'
+
+def TurnOnSwitch():
+    print 'Switch is on'
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(2, GPIO.OUT)
+    #GPIO.output(2, False)
+    return 'On'
