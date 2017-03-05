3bd56d8adf4bec88a9a1f1486a9ee7409c991707
Nic 7aller <n.7aller@gmail.com>
2017-03-05 06:36:40 -0500

    Added weather

--- BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
+++ BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
@@ -5,8 +5,15 @@ Routes and views for the flask application along with switch functions.
 from flask import render_template
 from BipFlaskProject import app
 from LightSwitch import LightSwitch
+import forecastio
 
 lightSwitch = LightSwitch()
+
+api_key = "e59dd0570c87555e9219188605bb324c"
+lat = -31.967819
+lng = 40.00404
+
+forecast = forecastio.load_forecast(api_key, lat, lng)
 
 @app.route('/')
 @app.route('/home')
@@ -15,6 +22,7 @@ def home():
     return render_template(
         'index.html',
         state = lightSwitch.lightState,
+        weather = forecast.currently().summary
     )
 
 @app.route('/about')
@@ -31,4 +39,5 @@ def turnSwitch():
     return render_template(
         'index.html',
         state = lightSwitch.lightState,
+        weather = forecast.currently().summary
     )
