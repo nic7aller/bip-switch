d8b8af692cbf43c525f0f6f3986c98eb56541c8b
Nic 7aller <n.7aller@gmail.com>
2017-03-04 22:15:14 -0500

    Added LightSwitch class to handle light Switch changes

---
+++ BipFlaskProject/BipFlaskProject/BipFlaskProject/LightSwitch.py
@@ -0,0 +1,28 @@
+#import RPi.GPIO as GPIO
+
+class LightSwitch(object):
+    """Controls the light switch"""
+    
+    def __init__(self):
+        self.lightState = 'Off'
+
+    def ChangeLightState(self):
+        if self.lightState == 'On':
+            self.lightState = self.TurnOffSwitch()
+        elif self.lightState == 'Off':
+            self.lightState = self.TurnOnSwitch()
+        return
+
+    def TurnOffSwitch(self):
+        print 'Switch is off'
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(2, GPIO.OUT)
+        #GPIO.output(2, True)
+        return 'Off'
+
+    def TurnOnSwitch(self):
+        print 'Switch is on'
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(2, GPIO.OUT)
+        #GPIO.output(2, False)
+        return 'On'
--- BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
+++ BipFlaskProject/BipFlaskProject/BipFlaskProject/views.py
@@ -5,10 +5,9 @@ Routes and views for the flask application along with switch functions.
 from datetime import datetime
 from flask import render_template
 from BipFlaskProject import app
-#import RPi.GPIO as GPIO
-import os
+from LightSwitch import LightSwitch
 
-lightState = 'Off'
+lightSwitch = LightSwitch()
 
 @app.route('/')
 @app.route('/home')
@@ -16,9 +15,9 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-        state=lightState,
+        title = 'Home Page',
+        year = datetime.now().year,
+        state = lightSwitch.lightState,
     )
 
 @app.route('/contact')
@@ -26,9 +25,9 @@ def contact():
     """Renders the contact page."""
     return render_template(
         'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
+        title = 'Contact',
+        year = datetime.now().year,
+        message = 'Your contact page.'
     )
 
 @app.route('/about')
@@ -36,50 +35,18 @@ def about():
     """Renders the about page."""
     return render_template(
         'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
+        title = 'About',
+        year = datetime.now().year,
+        message = 'Your application description page.'
     )
 
-@app.route('/TurnOffSwitch')
-def turnOff():
-    """Turns off switch"""
-    ChangeLightState(0)
+@app.route('/TurnSwitch')
+def turnSwitch():
+    """Turns on or off switch"""
+    lightSwitch.ChangeLightState()
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-        state=lightState,
+        title = 'Home Page',
+        year = datetime.now().year,
+        state = lightSwitch.lightState,
     )
-
-@app.route('/TurnOnSwitch')
-def turnOn():
-    """Turns on switch"""
-    ChangeLightState(1)
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-        state=lightState,
-    )
-
-def ChangeLightState(onOrOff):
-    if onOrOff == 0:
-        lightState = TurnOffSwitch()
-    elif onOrOff == 1:
-        lightState = TurnOnSwitch()
-    return
-
-def TurnOffSwitch():
-    print 'Switch is off'
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setup(2, GPIO.OUT)
-    #GPIO.output(2, True)
-    return 'Off'
-
-def TurnOnSwitch():
-    print 'Switch is on'
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setup(2, GPIO.OUT)
-    #GPIO.output(2, False)
-    return 'On'
